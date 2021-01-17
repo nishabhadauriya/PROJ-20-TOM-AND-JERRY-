7b924ed0826b64fd900879c2436a91085a9fb70f
nishabhadauriya <68988142+nishabhadauriya@users.noreply.github.com>
2021-01-17 21:55:19 +0530

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,7 +1,7 @@
 var canvas,bg;
 var together;
-var tom, tomImg1,tomImg2,tomImg3;
-var jerry, jerryImg1,jerryImg2,jerryImg3;
+var tom, tomImg1,tomImg23,tomImg4;
+var jerry, jerryImg1,jerryImg23,jerryImg4;
 
 function preload() {
     bg = loadImage("images/garden.png");
